95e35d2ddbfad8057d5d73c17defeb6d37519bba
Lord Turmoil <tony-turmoil@outlook.com>
2024-06-16 18:07:40 +0800

    init commit

---
+++ MergePdf/PdfHelper.cs
@@ -0,0 +1,38 @@
+﻿using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+
+namespace MergePdf;
+
+static class PdfHelper
+{
+    public static void Merge(string output, string[] inputs)
+    {
+        if (inputs.Length == 0)
+        {
+            Console.WriteLine("No input files specified.");
+            return;
+        }
+
+        foreach (string input in inputs)
+        {
+            Console.WriteLine($"Merging {input}");
+            if (!File.Exists(input))
+            {
+                Console.WriteLine($"File not found: {input}");
+                return;
+            }
+        }
+
+        using var document = new PdfDocument();
+        foreach (var input in inputs)
+        {
+            using var inputDocument = PdfReader.Open(input, PdfDocumentOpenMode.Import);
+            for (var i = 0; i < inputDocument.PageCount; i++)
+            {
+                var page = inputDocument.Pages[i];
+                document.AddPage(page);
+            }
+        }
+        document.Save(output);
+    }
+}
---
+++ MergePdf/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace MergePdf;
+
+/// <summary>
+/// Usage:
+///     MergePdf <output> <input1> <input2> ...
+///     MergePdf <output> -r <input>
+/// </summary>
+class Program
+{
+    private static void Main(string[] args)
+    {
+        if (args.Length < 2 || (args[1] == "-r" && args.Length != 3))
+        {
+            Console.WriteLine("Usage:");
+            Console.WriteLine("    MergePdf <output> <input1> <input2> ...");
+            Console.WriteLine("    MergePdf <output> -r <input>");
+            return;
+        }
+
+        var output = args[0];
+        var isRecursive = args[1] == "-r";
+
+        if (isRecursive)
+        {
+            var files = Directory.GetFiles(args[2], "*.pdf", SearchOption.TopDirectoryOnly);
+            PdfHelper.Merge(output, files);
+        }
+        else
+        {
+            var files = args.Skip(1).ToArray();
+            PdfHelper.Merge(output, files);
+        }
+    }
+}
